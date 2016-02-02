b33f981fdf40d699d19d1faa78dff7bbc43049a5
Rochelle Willard <rwillard@usc.edu>
2016-02-01 19:06:03 -0800

    demo route

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/demo', function (req, res) {
+  res.sendFile(path.join(__dirname + '/demo.html'));
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
