c13bd54fc5d7aa475e3ae40acdcd7172d6d61631
Rochelle Willard <rwillard@usc.edu>
2016-02-01 19:28:14 -0800

    babel compile for demo page

---
+++ src/demo.js
@@ -0,0 +1,24 @@
+import React, { PropTypes, Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Hello extends Component {
+  static propTypes = {
+    user: PropTypes.string
+  };
+
+  static defaultProps = {
+    user: 'World'
+  };
+
+  render() {
+    const { user } = this.props;
+    return (<h1>Demo {user}</h1>);
+  }
+}
+
+
+ReactDOM.render(
+  <Hello user="World"/>,
+  document.getElementById('app')
+);
+
