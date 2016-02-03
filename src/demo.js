74ae33ed347e9e97e642ca3be10f3de2d3a55be9
Rochelle Willard <rwillard@usc.edu>
2016-02-02 22:18:27 -0800

    slider jank version, set for window width 780px

--- src/demo.js
+++ src/demo.js
@@ -132,6 +132,84 @@ DemoPane2.styles = {
   }
 };
 
+//a container for the before and after of the same picture
+export default class DemoPane3 extends Component {
+  //props for before and after pictures
+  static propTypes = {
+    before: PropTypes.string,
+    after: PropTypes.string,
+  };
+
+  //default prop values
+  static defaultProps = {
+    before: '',
+    after: '',
+  };
+
+  //state variables including whether after picture has been pressed
+  state = {sliderPosition:393.844};
+
+  //when after picture has been clicked, reverse its state
+  handleClicks(e) {
+    this.setState({sliderPosition: e.pageX});
+  };
+
+  render() {
+    //consts for props
+    const { before, after } = this.props;
+    var styles = this.constructor.styles;
+
+    //decide which picture to render on the right based on whether
+    //or not is has been clicked
+    var pic = this.state.pressed ? before : after;
+    var style1 = {backgroundImage: "url(" + after + ")"};
+    var style2 = {backgroundImage: "url(" + before + ")", width: ((this.state.sliderPosition-30)/7.2788) + "%"};
+    var style3 = {left: ((this.state.sliderPosition-30)/7.2788) + "%"};
+    return (
+      <div style={styles.picContainer}>
+        <div style={{...styles.pic, ...style1}} onClick={this.handleClicks.bind(this)}>
+          <div style={{...styles.divisor, ...style2}}></div>
+          <div style={{...styles.slider, ...style3}} />
+        </div>
+      </div>
+    );
+  }
+}
+
+//styling for Demo Pane
+DemoPane3.styles = {
+  picContainer: {
+    display: "inline-block",
+    marginTop: "20px",
+    textAlign: "center",
+    width: "100%"
+  },
+  pic: {
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    cursor: "pointer",
+    display: "block",
+    height: "500px",
+    margin: "auto",
+    position: "relative",
+    width: "95%"
+  },
+  divisor: {
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    bottom: "0",
+    height: "100%",
+    overflow: "hidden",
+    position: "absolute",
+  },
+  slider: {
+    backgroundColor: "black",
+    height: "100%",
+    position: "absolute",
+    width: "10px"
+  }
+};
+
 
 //container for all of the components of the Demo page
 export default class DemoPage extends Component{
@@ -144,6 +222,7 @@ export default class DemoPage extends Component{
         <DemoPane before="./img/building-before.png" after="./img/building-after.png"/>
         <DemoPane2 before="./img/bears-before.png" after="./img/bears-after.png"/>
         <DemoPane2 before="./img/building-before.png" after="./img/building-after.png"/>
+        <DemoPane3 before="./img/bears-before.png" after="./img/bears-after.png"/>
       </div>
     );
   }
