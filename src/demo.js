904f5b4e551e72d4dc53e9bec77e8be0653f8d63
Rochelle Willard <rwillard@usc.edu>
2016-02-02 21:10:41 -0800

    version 2

--- src/demo.js
+++ src/demo.js
@@ -36,9 +36,11 @@ export default class DemoPane extends Component {
     return (
       <div>
         <div style={styles.picContainer}>
+          <p>Before</p>
           <img style={styles.pic} src={before}/>
         </div>
-        <div style={styles.picContainer}>
+        <div style={{...styles.picContainer, ...styles.afterPic}}>
+          <p>After</p>
           <img style={styles.pic} src={pic} onMouseDown={this.handleClick.bind(this)} 
               onMouseUp={this.handleClick.bind(this)}/> 
         </div>
@@ -51,25 +53,105 @@ export default class DemoPane extends Component {
 DemoPane.styles = {
   picContainer: {
     display: "inline-block",
+    textAlign: "center",
     width: "50%"
   },
   pic: {
     display: "block",
     margin: "auto",
     width: "95%"
+  },
+  afterPic: {
+    cursor: "pointer"
   }
 };
+
+//a container for the before and after of the same picture
+export default class DemoPane2 extends Component {
+  //props for before and after pictures
+  static propTypes = {
+    before: PropTypes.string,
+    after: PropTypes.string,
+  };
+
+  //default prop values
+  static defaultProps = {
+    before: '',
+    after: '',
+  };
+
+  //state variables including whether after picture has been pressed
+  state = {pressed:false};
+
+  //when after picture has been clicked, reverse its state
+  handleClick() {
+    this.setState({pressed: !this.state.pressed});
+  };
+
+  render() {
+    //consts for props
+    const { before, after } = this.props;
+    var styles = this.constructor.styles;
+
+    //decide which picture to render on the right based on whether
+    //or not is has been clicked
+    var pic = this.state.pressed ? before : after;
+    return (
+      <div style={styles.picContainer}>
+        <img style={styles.pic} src={pic} onMouseDown={this.handleClick.bind(this)} 
+            onMouseUp={this.handleClick.bind(this)}/> 
+        <div style={styles.buttonPanel}>
+          <button style={styles.button}>Low</button>
+          <button style={styles.button}>Medium</button>
+          <button style={styles.button}>High</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+//styling for Demo Pane
+DemoPane2.styles = {
+  picContainer: {
+    display: "inline-block",
+    marginTop: "20px",
+    textAlign: "center",
+    width: "100%"
+  },
+  pic: {
+    cursor: "pointer",
+    display: "block",
+    margin: "auto",
+    width: "95%"
+  },
+  buttonPanel: {
+    marginTop: "10px"
+  },
+  button: {
+    margin: "10px"
+  }
+};
+
 
 //container for all of the components of the Demo page
 export default class DemoPage extends Component{
   render(){
+    var styles = this.constructor.styles;
     return(
-      <div>
-        <p> Check it out! </p>
-        <DemoPane before="./img/bears-before.png" after="./img/bears-after.png"/>,
-        <DemoPane before="./img/building-before.png" after="./img/building-after.png"/>,
+      <div style={styles.page}>
+        <p>Press and Hold the Popped Image to compare to the original</p>
+        <DemoPane before="./img/bears-before.png" after="./img/bears-after.png"/>
+        <DemoPane before="./img/building-before.png" after="./img/building-after.png"/>
+        <DemoPane2 before="./img/bears-before.png" after="./img/bears-after.png"/>
+        <DemoPane2 before="./img/building-before.png" after="./img/building-after.png"/>
       </div>
     );
+  }
+}
+
+DemoPage.styles = {
+  page: {
+    textAlign: "center"
   }
 }
 
