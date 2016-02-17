e5e6d5c671f33c35c25effc57156654bbd5b578c
Bernard Xie <bxie94@gmail.com>
2016-02-17 13:46:30 -0800

    finishd mocked backend api

---
+++ routes/api/backend.js
@@ -0,0 +1,25 @@
+var express = require('express');
+router = express.Router();
+
+router.get('/', function (req, res, next) {
+    res.send([
+        {
+            "name": "apiVersion",
+            "value": "1.0.0"
+        },
+        {
+            "name": "uptime",
+            "value": "13 hours"
+        },
+        {
+            "name": "processId",
+            "value": "4612"
+        },
+        {
+            "name": "serverHostName",
+            "value": "localhost"
+        }
+    ]);
+})
+
+module.exports = router;
