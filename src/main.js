0f7dd54b2e76f50f14a3f3a53446828c148ef37b
Ananth Mohan <ananth.m.95@gmail.com>
2016-02-21 17:34:32 -0800

    Basic implementation of list and image selection

--- src/main.js
+++ src/main.js
@@ -1,80 +1,117 @@
 import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
 import Dropzone from 'react-dropzone';
-import styles from '../css/mainPage.css';
+import styles from '../css/main-page/mainPage.css';
 
 export default class ImageViewArea extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {imageUrl: props.imageUrl};
-  }
-
-  static propTypes = {
-    imageUrl: PropTypes.string,
-  }
-
-  defaultProps = {
-    imageUrl: null,
-  };
-};
-
-export default class FileListElement extends Component {
   constructor(props) {
     super(props);
   }
 
   static propTypes = {
     file: PropTypes.object,
+  }
+
+  defaultProps = {
+    file: null,
   };
 
   render = () => {
     const {file} = this.props;
     return (
-      <div className='fileListElement'>
-        <div className='listElementIconWrapper'>
-          <img className='listElementIcon' src={file.preview} />
-        </div>
-        <div className='listElementName'>{file.name}</div>
+      <div className='imageViewArea'>
+        {file == null ?
+          <div>Upload images to view more options!</div> :
+          <img className='fullImageView' src={file.preview} />
+        }
       </div>
     );
   }
 };
 
-export default class DropzoneListArea extends Component {
+export default class FileListElement extends Component {
   constructor(props) {
     super(props);
-    this.state = {files: []};
+    this.state = {selected: false};
+  }
+
+  static propTypes = {
+    file: PropTypes.object,
+  };
+
+  onListElementClick = (file) => {
+    this.props.onListElementClick(file);
+    this.setState({
+      selected: true
+    });
   }
 
   render = () => {
+    const {file} = this.props;
     return (
-      <div id='dropzoneListArea'>
-        <div className='centered'>
-          <h1 className='centered'>My Images</h1>
-          <div>
-            Drag and drop images anywhere to upload or
-            <button type='button' onClick={this.props.onOpenClick}>
-              Click Here!
-            </button>
+      <div className='fileListElement' onClick={this.onListElementClick.bind(this, file)}>
+
+          <div className='listElementIconWrapper'>
+            <img className='listElementIcon' src={file.preview} />
           </div>
+          <div className='listElementName'>{file.name}</div>
+      </div>
+    );
+  }
+};
+
+export default class ImageListArea extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {fileId: 0}
+  }
+
+  render = () => {
+    const {files, onOpenClick, onListElementClick} = this.props;
+
+    return (
+      <div className='imageListArea'>
+        <div className='centered imageListAreaHeader'>
+          <h1 className='centered'>My Images</h1>
+          Drag and drop images anywhere to upload or <br />
+          <button type='button' onClick={onOpenClick}>
+            Click Here!
+          </button>
+        </div>
+        <div className='imageList'>
+          {files.map((file) =>
+              <FileListElement file={file} key={file.fileId}
+                               onListElementClick={onListElementClick} />)
+          }
         </div>
       </div>
     );
   }
 };
 
-export default class MainPageArea extends Component {
+export default class MainPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {files: []}
+    this.state = {files: [], selectedFile: null}
   }
 
+  fileId = 0;
+
   onDrop = (files) => {
-    this.setState({
-      files: files
+    files.forEach(function(file) {
+      file.fileId = this.fileId++;
+    }, this);
+
+    this.setState(function(prevState, currProps) {
+      return {files: prevState.files.concat(files)};
     });
-    console.log(files);
-  };
+  }
+
+  onListElementClick = (file) => {
+    this.setState(function(prevState, currProps) {
+      return {selectedFile: file};
+    });
+  }
 
   onOpenClick = () => {
     this.refs.dropzone.open();
@@ -82,9 +119,17 @@ export default class MainPageArea extends Component {
 
   render = () => {
     return (
-      <Dropzone className='dropzoneArea' ref='dropzone' onDrop={this.onDrop}
-                disableClick={true} accept={"image/gif,image/jpeg,image/png"}>
-        <DropzoneListArea files={this.state.files} onOpenClick={this.onOpenClick}/>
+      <Dropzone className='dropzoneArea' activeClassName='dropzoneArea dropzoneAreaActive'
+                ref='dropzone' onDrop={this.onDrop} disableClick={true}
+                accept={"image/gif,image/jpeg,image/png"}>
+        <div className='dropzoneOverlay'>
+          <h1 className='dropzoneOverlayText'>
+            Drop File to Upload!
+          </h1>
+        </div>
+        <ImageListArea files={this.state.files} onOpenClick={this.onOpenClick}
+                       onListElementClick={this.onListElementClick} />
+        <ImageViewArea file={this.state.selectedFile}/>
       </Dropzone>
     );
   }
@@ -92,6 +137,6 @@ export default class MainPageArea extends Component {
 
 
 ReactDOM.render(
-  <MainPageArea />,
+  <MainPage />,
   document.getElementById('app')
 );
