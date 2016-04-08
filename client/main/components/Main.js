fda88036a5b099d8b76b0cac421fe1b796cee59c
Matthew Burke <matthelb@usc.edu>
2016-04-08 01:55:00 -0700

    Main page Finder and Editor

--- client/main/components/Main.js
+++ client/main/components/Main.js
@@ -7,6 +7,8 @@ import PageTemplate from '../../shared/components/PageTemplate.js';
 import { Modal, Grid, Row, Col, Button } from 'react-bootstrap';
 import request from 'superagent-bluebird-promise';
 import Config from 'Config';
+import SplitPane from 'react-split-pane';
+import DrawableCanvas from 'react-drawable-canvas';
 
 export class ImageControlArea extends Component {
   handleSlider = (value) => {
@@ -171,6 +173,167 @@ export class File {
   }
 }
 
+export class ImageTableRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+    };
+  }
+
+  static propTypes = {
+    file: PropTypes.object,
+    selected: PropTypes.bool,
+    uploaded: PropTypes.bool,
+    progress: PropTypes.number,
+  };
+
+
+  onListElementClick = (file) => {
+    if (this.props.uploaded) {
+      this.props.onListElementClick(file);
+      this.setState({
+        selected: true
+      });
+    }
+  };
+
+  render = () => {
+    const { file } = this.props;
+    var size = file.size / 1024;
+    if (size >= 1024) {
+      size = (Math.round(size / 1024 * 100) / 100) + ' MB';
+    } else {
+      size = (Math.round(size * 100) / 100) + ' KB';
+    }
+    return (
+      <tr onClick={this.onListElementClick.bind(this, file)} className='imageTableRow'>
+        <td className='imageTableRowName'>
+          <img className='imageTableRowIcon' src={file.preview}/>{file.name}
+        </td>
+        <td className='imageTableRowSize'>{size}</td>
+      </tr>
+    );
+  };
+
+};
+
+export class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brushColor: '#800909',
+      lineWidth: 4,
+      canvasStyle: {
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        pointerEvents: 'none',
+      },
+      clear: false
+    };
+  }
+
+  static propTypes = {
+    file: PropTypes.object,
+  };
+
+  handleSlider = (value) => {
+    console.log(value);
+  };
+
+  handleOnClickReset = () => {
+    this.setState({
+      canvasStyle: {
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        pointerEvents: 'none',
+      },
+      clear: true
+    });
+  };
+
+  handleOnClickTouchUp = () => {
+    this.setState({
+      brushColor: '#CCCCCC',
+      canvasStyle: {
+        backgroundColor: 'rgba(0, 0, 0, 0)',
+        pointerEvents: 'auto',
+      },
+      clear: false
+    });
+  };
+
+  render = () => {
+    const {file} = this.props;
+    return file == undefined ? (<div className='editor'>Select an image</div>) : (
+      <div className='editor'>
+        <div className='editorContainer'>
+          <div className='editorImageContainer'>
+            <img src={file.preview} className='editorImage'/>
+          </div>
+          <div className='canvasContainer'>
+            <DrawableCanvas {...this.state}/>
+          </div>
+        </div>
+        <div className='toolbox'>
+          <Button onClick={this.handleOnClickTouchUp}>Touch Up</Button>
+          <Button onClick={this.handleOnClickReset}>Reset</Button>
+          <div className='sliderWrapper'>
+            <Slider defaultValue={2} min={1} max={3} step={1} withBars
+              onChange={this.handleSlider}>
+              <div className='handle'/>
+            </Slider>
+            <div className='label labelLeft'>low</div>
+            <div className='label'>med</div>
+            <div className='label labelRight'>high</div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+ti
+};
+
+export class ImagesTable extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static propTypes = {
+    files: React.PropTypes.array,
+    onOpenClick: React.PropTypes.func,
+    onListElementClick: React.PropTypes.func
+  };
+
+  static defaultProps = {
+    files: [],
+  };
+
+  render = () => {
+    return (
+    <div style={{overflowY: 'auto', maxHeight: 'calc(100vh - 50px)'}}>
+      <table className='imagesTable'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Size</th>
+          </tr>
+        </thead>
+        <tbody className='imagesTableBody'>
+        {this.props.files.map((file) =>
+          <ImageTableRow file={file} key={file.fileId}
+            selected={this.props.selectedFile != null &&
+              this.props.selectedFile.fileId == file.fileId}
+            onListElementClick={this.props.onListElementClick}
+            uploaded={file.progress == 1}
+            progress={file.progress} />)
+        }
+        </tbody>
+      </table>
+    </div>
+      );
+  };
+
+};
+
 export class MainPageContent extends Component {
   constructor(props) {
     super(props);
@@ -188,11 +351,8 @@ export class MainPageContent extends Component {
         .then(function(res) {
           let resJson = JSON.parse(res.text);
           let file = new File(raw_file, resJson.fileId);
-          files.push(file);
-
-          self.setState(function(prevState, currProps) {
-            return {files: prevState.files.concat(files)};
-          });
+          file.progress = 1;
+          self.setState({files: self.state.files.concat([file])});
 
           var promise = request
             .post(Config.apiHost + '/com/imagepop/fileupload/upload')
@@ -200,10 +360,10 @@ export class MainPageContent extends Component {
             .field('fileId', file.fileId)
             .attach('image', raw_file)
             .on('progress', function (p) {
-                file.progress = p.loaded / p.total;
+                /*file.progress = p.loaded / p.total;
                 self.setState(function(prevState, currProps) {
                   return {files: prevState.files};
-                });
+                });*/
             })
             .promise()
             .then(function(res) {
@@ -229,20 +389,16 @@ export class MainPageContent extends Component {
       <Dropzone className='container-fluid' activeClassName='dropzoneArea dropzoneAreaActive'
                 ref='dropzone' onDrop={this.onDrop} disableClick={true}
                 accept={"image/gif,image/jpeg,image/png"}>
-        <Row>
-          <div className='dropzoneOverlay'>
-            <h1 className='dropzoneOverlayText'>
-              Drop File to Upload!
-            </h1>
-          </div>
-          <Col sm={4}>
-            <ImageListArea selectedFile={this.state.selectedFile} files={this.state.files}
-                onOpenClick={this.onOpenClick} onListElementClick={this.onListElementClick} />
-          </Col>
-          <Col sm={8}>
-            <SelectedImageArea file={this.state.selectedFile}/>
-          </Col>
-        </Row>
+        <div className='dropzoneOverlay'>
+          <h1 className='dropzoneOverlayText'>
+            Drop File to Upload!
+          </h1>
+        </div>
+        <SplitPane split='vertical' defaultSize={'30%'}>
+          <ImagesTable selectedFile={this.state.selectedFile} files={this.state.files}
+            onOpenClick={this.onOpenClick} onListElementClick={this.onListElementClick} />
+          <Editor file={this.state.selectedFile}/>
+        </SplitPane>
       </Dropzone>
     );
   };
@@ -251,7 +407,7 @@ export class MainPageContent extends Component {
 export default class Main extends Component {
   render = () => {
     return (
-      <PageTemplate title="Main" subtitle="Upload an image for popping.">
+      <PageTemplate>
         <MainPageContent/>
       </PageTemplate>
     );
